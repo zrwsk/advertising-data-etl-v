7343bda86503de42396bff73a04c1b632972d7ba
Jakub Żurawski <kuba@MBP.local>
2021-07-30 22:52:12 +0200

    Add basic linting

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,21 +1,21 @@
-import { Box } from "@material-ui/core";
-import { LineChart, CartesianGrid, XAxis, YAxis, Legend, Line, ResponsiveContainer } from "recharts";
+import { Box } from '@material-ui/core';
+import {
+  LineChart, CartesianGrid, XAxis, YAxis, Legend, Line, ResponsiveContainer,
+} from 'recharts';
 
-const Chart = (): JSX.Element => {
-  return (
-    <Box mt={4} pr={8}>
-      <ResponsiveContainer width="100%" height={400}>
-        <LineChart data={[]}>
-          <CartesianGrid vertical={false} />
-          <XAxis />
-          <YAxis />
-          <Legend />
-          <Line type="monotone" dataKey="Clicks" stroke="#8884d8" />
-          <Line type="monotone" dataKey="Impressions" stroke="#82ca9d" />
-        </LineChart>
-      </ResponsiveContainer>
-    </Box>
-  );
-};
+const Chart = (): JSX.Element => (
+  <Box mt={4} pr={8}>
+    <ResponsiveContainer width="100%" height={400}>
+      <LineChart data={[]}>
+        <CartesianGrid vertical={false} />
+        <XAxis />
+        <YAxis />
+        <Legend />
+        <Line type="monotone" dataKey="Clicks" stroke="#8884d8" />
+        <Line type="monotone" dataKey="Impressions" stroke="#82ca9d" />
+      </LineChart>
+    </ResponsiveContainer>
+  </Box>
+);
 
 export default Chart;
